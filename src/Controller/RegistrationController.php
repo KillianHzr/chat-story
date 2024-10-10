b3a6737e61219fcc4e40a1c30e7907b93cf45225
KillianHzr <killianherzer@gmail.com>
2024-10-10 16:38:00 +0200

    ADD [admin] Dashboard and rework login / register logic

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -31,7 +31,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
