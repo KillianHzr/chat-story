1f555421b33401e5b468c6baec627eed1f412176
Axel Genin <axel.genin17@gmail.com>
2024-10-11 11:42:14 +0200

    add chat gpt correct text json format

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\ChatGptAI;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,18 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ChatGptAI $chatGptAI): Response
     {
+        $chatGptAI->requestChatGPT("genère moi une liste de chien", []);
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
+
+
+
+
+
 }
--- src/Service/ChatGptAI.php
+++ src/Service/ChatGptAI.php
@@ -17,19 +17,53 @@ class ChatGptAI
 
     public function requestChatGPT(string $prompt, array $options = []): array
     {
-        $response = $this->client->request('POST', 'https://api.openai.com/v1/completions', [
+        $response = $this->client->request('POST', 'https://api.openai.com/v1/chat/completions', [
             'headers' => [
                 'Authorization' => 'Bearer ' . $this->apiKey,
                 'Content-Type' => 'application/json',
             ],
             'json' => array_merge([
-                'model' => 'text-davinci-003',
-                'prompt' => $prompt,
-                'max_tokens' => 1000,
+                'model' => 'gpt-4o-2024-08-06',
+                'messages' => [
+                    ['role' => 'user', 'content' => 'Are semicolons optional in JavaScript?'],
+                    ['role' => 'system', 'content' => "Tu es un assistant qui permet d'écrire un histoire à choix multiples. Ton rôle est narrateur, ton but est de rédiger des parties d'histoires puis de proposer des choix qui guideront la suite de l'histoire. 
+                    L'histoire doit être cohérente et suivre une chronologie logique. Le thème doit être le même. A la fin de chaque partie, tu dois proposer 3 choix. 
+                    Renvoie un string au format JSON, avec un champ 'choices' (tableau de string) un champ 'story' (string) "]
+                ],
                 'temperature' => 0.7,
+                'response_format' => [
+                    'type' => 'json_schema',
+                    'json_schema' => [
+                        'name' => 'sdfs',
+                        'schema' => [
+                            'type' => 'object',
+                            'properties' => [
+                                'choices'=> [
+                                    'type' => 'array',
+                                    'maxItems' => 3,
+                                    'items' => [
+                                        'type' => 'string',
+                                    ]
+                                ],
+                                'story' => [
+                                    'type' => 'string'
+                                ]
+                            ]
+                         ]
+                    ]
+                ]
             ], $options)
         ]);
 
-        return $response->toArray();
+        try {
+            $result = $response->toArray();
+            $content = (string) $result['choices'][0]['message']['content'];
+            dump($content);
+            return [];
+        }
+        catch (\Exception $e) {
+            dump($e);
+            return [];
+        }
     }
 }
