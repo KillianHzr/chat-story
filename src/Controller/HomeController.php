c82b4f0298e3b9636f47e324e2f866b9e1041ea2
KillianHzr <killianherzer@gmail.com>
2024-10-11 14:57:45 +0200

    ADD [front] Page room unique

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Room;
 use App\Service\ChatGptAI;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,17 @@ class HomeController extends AbstractController
        dump($result);
         $result = $chatGptAI->continue($result['choices'][0] );
         dump($result);
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+
+        ]);
+    }
+
+    #[Route('/room/{id}', name: 'app_room')]
+    public function room(Room $room, ChatGptAI $chatGptAI): Response
+    {
+        
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
